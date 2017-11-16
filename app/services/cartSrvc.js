206b7edc1b8a2ba627c5bb363e03f0ca44b8ac2b
Kedirech <brackcarmony@gmail.com>
2017-11-16 09:54:57 -0700

    push

--- app/services/cartSrvc.js
+++ app/services/cartSrvc.js
@@ -1,12 +1,5 @@
 angular.module('ecommerce').service('cartSrvc', function(){
-  let cart = [{
-        amount:1,
-        product:{name:'Space Fruit',
-        imageUrl:'https://ctl.s6img.com/society6/img/TtK7KWlA4hPvW98_g3IMjwK5vXc/h_550,w_550/tshirts/men/greybg/black/~artwork,bg_FFFFFFFF/s6-0025/a/11385279_14899658/~~/lunar-fruit-tshirts.jpg',
-        description:'These stylish shoes are great for throwing at people.  Holding pencils.  Also could be worn on feet.',
-        price:30
-      }}
-  ];
+  let cart = [];
 
   this.addItem = function(newItem){
     cart.push({
